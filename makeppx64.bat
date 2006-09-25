cdbec31b040e879f1fb77288522d2184696df84a
florian <florian@b4dd5b90-57f8-0310-8fd5-d8d89816a767>
2006-09-25 16:56:09 +0000

    * small script changes

--- makeppx64.bat
+++ makeppx64.bat
@@ -1,1 +1,1 @@
-make FPC=c:\fpc\compiler\x86_64\pp "OPT=-Fuc:\fpc\rtl\units\x86_64-win64" %1 %2 %3 %4 %5 %6 %7 %8 %9
+make FPC=c:\fpc\compiler\x86_64\pp "OPT=@c:\fpc\bin\fpc.cfg" %1 %2 %3 %4 %5 %6 %7 %8 %9
