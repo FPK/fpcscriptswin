b3f369d8217aaca3a23ea8e822ee9552e0dd6c59
florian <florian@b4dd5b90-57f8-0310-8fd5-d8d89816a767>
2006-11-02 18:25:27 +0000

    * uses fpc.cfg now as well

--- pp11.bat
+++ pp11.bat
@@ -1,1 +1,1 @@
-c:\fpc\compiler\pp -n -Fuc:\fpc\rtl\units\i386-win32 -Fuc:\fpc\fcl\units\i386-win32 -Fuc:\fpc\packages\extra\winunits\units\i386-win32 %1 %2 %3 %4 %5 %6 %7 %8 %9
+c:\fpc\compiler\pp -n @c:\fpc\bin\fpc.cfg %1 %2 %3 %4 %5 %6 %7 %8 %9
