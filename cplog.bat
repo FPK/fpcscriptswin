baba9a6398c16b16b2d40e184585cb894aff0f0a
florian <florian@b4dd5b90-57f8-0310-8fd5-d8d89816a767>
2008-09-14 16:37:58 +0000

    * scripts updated

--- cplog.bat
+++ cplog.bat
@@ -1,2 +1,3 @@
 cp output/i386-win32/log log.%1
 cp output/i386-win32/longlog longlog.%1
+cp output/i386-win32/faillist faillist.%1
--- cplog64.bat
+++ cplog64.bat
@@ -1,2 +1,3 @@
 cp output/x86_64-win64/log log.%1
 cp output/x86_64-win64/longlog longlog.%1
+cp output/x86_64-win64/faillist faillist.%1
