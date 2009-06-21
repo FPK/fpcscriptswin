ffcef8feb23bef60b2c141a5845be116112f30bb
florian <florian@b4dd5b90-57f8-0310-8fd5-d8d89816a767>
2009-06-21 20:00:14 +0000

    * new and modified scripts

---
+++ fm.bat
@@ -0,0 +1,1 @@
+c:\fpc\utils\fpcm\fpcmake %1 %2 %3 %4 %5 %6 %7 %8 %9
--- makepp11.bat
+++ makepp11.bat
@@ -1,1 +1,1 @@
-make FPC=c:\fpc\compiler\pp "OPT=-Fuc:\fpc\rtl\units\i386-win32" %1 %2 %3 %4 %5 %6 %7 %8 %9
+make FPC=c:\fpc\compiler\pp "OPT=@c:\fpc\bin\fpc.cfg" %1 %2 %3 %4 %5 %6 %7 %8 %9
