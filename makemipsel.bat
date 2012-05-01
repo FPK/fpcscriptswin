b70f7c9b083d1470f7c83c98f95cc95f86078af5
florian <florian@b4dd5b90-57f8-0310-8fd5-d8d89816a767>
2012-05-01 13:38:44 +0000

    * script updates

---
+++ makemipsel.bat
@@ -0,0 +1,1 @@
+make "FPC=c:\fpc\%MYFPC%compiler\mipsel\pp" "OPT=@c:\fpc\bin\fpc.cfg" %1 %2 %3 %4 %5 %6 %7 %8 %9
--- t64.bat
+++ t64.bat
@@ -1,1 +1,1 @@
-fptime make clean all digest TEST_DELTEMP=1 FPC=%CD%\..\compiler\ppcx64 TEST_FPC=%CD%\..\compiler\ppcx64 V=1 TEST_OPT=-O2
+fptime make clean all digest TEST_DELTEMP=1 FPC=%CD%\..\compiler\ppcx64 TEST_FPC=%CD%\..\compiler\ppcx64 V=1 TEST_OPT=-O3
