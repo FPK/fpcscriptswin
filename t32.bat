b15684bff66aef3deabe309ec663761bc631fd81
florian <florian@b4dd5b90-57f8-0310-8fd5-d8d89816a767>
2013-10-27 18:20:58 +0000

    * script update

--- t32.bat
+++ t32.bat
@@ -1,7 +1,6 @@
 set start=%time%
-make clean TEST_DELTEMP=1 TEST_FPC=%CD%\..\compiler\ppc386 V=1 TEST_OPT=-O4 %1 %2 %3 %4 %5 %6 %7 %8 %9
-make all TEST_DELTEMP=1 TEST_FPC=%CD%\..\compiler\ppc386 V=1 "TEST_OPT=-O4 -Fd" %1 %2 %3 %4 %5 %6 %7 %8 %9 -j 3
-make digest TEST_DELTEMP=1 TEST_FPC=%CD%\..\compiler\ppc386 V=1 TEST_OPT=-O4 %1 %2 %3 %4 %5 %6 %7 %8 %9
+make distclean TEST_DELTEMP=1 TEST_FPC=%CD%\..\compiler\ppc386 V=1 TEST_OPT=-O4 %1 %2 %3 %4 %5 %6 %7 %8 %9
+make full TEST_DELTEMP=1 TEST_FPC=%CD%\..\compiler\ppc386 V=1 "TEST_OPT=-O2 -Fd" %1 %2 %3 %4 %5 %6 %7 %8 %9 -j
 set end=%time%
 echo %start%
 echo %end%
