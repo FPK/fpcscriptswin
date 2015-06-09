409cdff6987fe75652796360d06e62c0f919e66e
florian <florian@b4dd5b90-57f8-0310-8fd5-d8d89816a767>
2015-06-09 20:52:55 +0000

    * script updates

--- brtlpp11.bat
+++ brtlpp11.bat
@@ -3,7 +3,7 @@ echo Missing suffix
 ) else (
 cd c:\fpc\%MYFPC%rtl\win32
 make distclean FPC=c:\fpc\%MYFPC%compiler\i386\pp PP=c:\fpc\compiler\i386\pp "OPT=@c:\fpc\bin\fpc.cfg -al %MYOPT386%"
-make all FPC=c:\fpc\%MYFPC%compiler\i386\pp PP=c:\fpc\compiler\i386\pp "OPT=@c:\fpc\bin\fpc.cfg -al %MYOPT386%" -j
+make all FPC=c:\fpc\%MYFPC%compiler\i386\pp PP=c:\fpc\%MYFPC%compiler\i386\pp "OPT=@c:\fpc\bin\fpc.cfg -al %MYOPT386%" -j
 cd ..
 rm -rf units.%1
 cp -a units units.%1
