a5b825be319f3567b0d928b10cd6da9a06d0400e
Florian Klämpfl <florian@freepascal.org>
2020-11-19 22:40:14 +0100

    * updated scripts

--- quick_dcommit.bat
+++ quick_dcommit.bat
@@ -1,7 +1,6 @@
 set TIMESTR=%time:~0,2%%time:~3,2%%time:~6,2%
 set BRANCH=%date:~06,4%%date:~3,2%%date:~0,2%%TIMESTR: =0%
 echo Going to commit %BRANCH%"  || exit /b % %ERRORLEVEL%
-pause
 "%GITBINDIR%\git" branch %BRANCH% || exit /b % %ERRORLEVEL%
 "%GITBINDIR%\git" checkout %BRANCH% || exit /b % %ERRORLEVEL%
 "%GITBINDIR%\git" commit -a %* || exit /b % %ERRORLEVEL%
