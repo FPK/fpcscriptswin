e36293b1da803946cbd1977daef239fceb2f7a78
Florian Klämpfl <florian@freepascal.org>
2021-07-12 20:21:12 +0200

    * update

--- ba64.bat
+++ ba64.bat
@@ -1,2 +1,2 @@
 make distclean -j
-fptime make all OVERRIDEVERSIONCHECK=1 %* -j "CROSSOPT=%MYOPTA64%" "OS_TARGET=linux" "CPU_TARGET=aarch64"
+fptime make all OVERRIDEVERSIONCHECK=1 %* -j "CROSSOPT=%MYOPTA64%" "OS_TARGET=win64" "CPU_TARGET=aarch64"
