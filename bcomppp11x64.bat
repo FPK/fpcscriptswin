cc600a76256205cd1b2269f7497a475b673d4828
florian <florian@freepascal.org>
2021-09-30 19:57:49 +0200

    * updates

---
+++ bcomppp11x64.bat
@@ -0,0 +1,11 @@
+if "%~1"== "" (
+echo Missing suffix
+) else (
+rem call brtlpp11.bat
+cd c:\fpc\%MYFPC%compiler
+make distclean OS_TARGET=win64 FPC=c:\fpc\%MYFPC%compiler\x86_64\pp PP=c:\fpc\compiler\x86_64\pp "OPT=@c:\fpc\bin\fpc.cfg -al %MYOPTX64%"
+make all OS_TARGET=win64 FPC=c:\fpc\%MYFPC%compiler\x86_64\pp PP=c:\fpc\%MYFPC%compiler\x86_64\pp "OPT=@c:\fpc\bin\fpc.cfg -al %MYOPTX64%" -j
+cd ..
+rm -rf units.%1
+cp -a compiler/x86_64/units units.%1
+)
