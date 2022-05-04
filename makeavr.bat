b47eb188a275583fec0c0855e1a97ee1cc5b807b
florian <florian@freepascal.org>
2022-05-04 23:11:45 +0200

    * updates

--- makeavr.bat
+++ makeavr.bat
@@ -1,1 +1,1 @@
-make FPC=c:\fpc\%MYFPC%compiler\avr\pp "OPT=@c:\fpc\bin\fpc.cfg" "SUBARCH=avr5" %1 %2 %3 %4 %5 %6 %7 %8 %9 -j
+make FPC=c:\fpc\%MYFPC%compiler\avr\pp "OPT=@c:\fpc\bin\fpc.cfg" "SUBARCH=avr5" %* -j
---
+++ makeavr6.bat
@@ -0,0 +1,1 @@
+make FPC=c:\fpc\%MYFPC%compiler\avr\pp "OPT=@c:\fpc\bin\fpc.cfg" "SUBARCH=avr6" %* -j
