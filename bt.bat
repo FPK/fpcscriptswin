02d4178e9724f0dee3e5c35689bda7c084c039e9
florian <florian@freepascal.org>
2023-04-02 22:53:32 +0200

    * updates

--- bt.bat
+++ bt.bat
@@ -3,3 +3,4 @@ call b.bat || exit /b %ERRORLEVEL%
 cd tests
 call t32.bat || exit /b %ERRORLEVEL%
 call storelog.bat || exit /b %ERRORLEVEL%
+cd ..
--- bt64.bat
+++ bt64.bat
@@ -1,5 +1,5 @@
 @echo off
-make distclean CPU_TARGET=x86_64 OS_TARGET=win64 CPU_SOURCE=x86_64 OS_SOURCE=win64 -j
+make distclean CPU_TARGET=x86_64 OS_TARGET=win64 CPU_SOURCE=x86_64 OS_SOURCE=win64 -j 
 call b64.bat || exit /b %ERRORLEVEL%
 cd tests
 call t64.bat || exit /b %ERRORLEVEL%
