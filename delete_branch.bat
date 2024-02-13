be5a7e92898dc1e2cfb15bafe32b5624e0838d3c
florian <florian@freepascal.org>
2024-02-13 17:59:06 +0100

    * updates

--- delete_branch.bat
+++ delete_branch.bat
@@ -1,4 +1,4 @@
 echo Going to delete branch %1
-"%GITBINDIR%\git" push git.florianklaempfl.de --delete %1
-"%GITBINDIR%\git" push gitlab.com/FPK2 --delete %1
+"%GITBINDIR%\git" push %PERSONAL_REMOTE% --delete %1
+"%GITBINDIR%\git" push %GITLAB_REMOTE% --delete %1
 "%GITBINDIR%\git" branch -D %1
