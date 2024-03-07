4bf07ce8f3848341d1a5e31469c18fe6315a52a4
florian <florian@freepascal.org>
2024-03-07 23:12:05 +0100

    * updates

--- bavr.bat
+++ bavr.bat
@@ -2,4 +2,4 @@ rm build-stamp.avr-embedded
 IF "%subarch%"=="" (
     set SUBARCH=avr5
 )
-fptime make all OVERRIDEVERSIONCHECK=1 %1 %2 %3 %4 %5 %6 %7 %8 %9 -j "CROSSOPT=%MYOPTAVR%" "OS_TARGET=embedded" "CPU_TARGET=avr" "SUBARCH=%SUBARCH%"
+fptime make all OVERRIDEVERSIONCHECK=1 %* -j "CROSSOPT=%MYOPTAVR%" "OS_TARGET=embedded" "CPU_TARGET=avr" "SUBARCH=%SUBARCH%"
