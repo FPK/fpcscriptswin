0477dfc6df7fa744f99eb2262596e5f9dcaeae63
florian <florian@freepascal.org>
2024-03-13 22:39:19 +0100

    * updates

--- bjvm.bat
+++ bjvm.bat
@@ -1,2 +1,2 @@
 rm build-stamp.jvm-java
-fptime make all OVERRIDEVERSIONCHECK=1 %1 %2 %3 %4 %5 %6 %7 %8 %9 -j "OPTNEW=%MYOPTJVM%" "OS_TARGET=java" "CPU_TARGET=jvm"
+fptime make all OVERRIDEVERSIONCHECK=1 %* -j "OPTNEW=%MYOPTJVM%" "OS_TARGET=java" "CPU_TARGET=jvm"
