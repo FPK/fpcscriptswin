d134495c1544d20d91ce82e72721983d9c8ace7f
florian <florian@freepascal.org>
2024-04-06 22:40:21 +0200

    * updates

--- makearm.bat
+++ makearm.bat
@@ -1,1 +1,1 @@
-make FPC=c:\fpc\%MYFPC%compiler\arm\pp OPT=@c:\fpc\bin\fpc.cfg %1 %2 %3 %4 %5 %6 %7 %8 %9
+make FPC=c:\fpc\%MYFPC%compiler\arm\pp OPT=@c:\fpc\bin\fpc.cfg %*
--- makefpc11.bat
+++ makefpc11.bat
@@ -1,1 +1,1 @@
-make PP=c:\fpc\%MYFPC%compiler\ppc386 "OPT=-n @c:\fpc\bin\fpc.cfg" %1 %2 %3 %4 %5 %6 %7 %8 %9
+make PP=c:\fpc\%MYFPC%compiler\ppc386 "OPT=-n @c:\fpc\bin\fpc.cfg" %*
