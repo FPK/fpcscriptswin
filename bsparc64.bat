88781fd748354539b38c3be6ba33d27d9a1fb025
florian <florian@freepascal.org>
2024-05-04 23:30:44 +0200

    * updates

--- bsparc64.bat
+++ bsparc64.bat
@@ -1,3 +1,3 @@
 make distclean -j
 make distclean -j
-fptime make all OVERRIDEVERSIONCHECK=1 %*-j "OPT=-O3" "OPTNEW=%MYOPTSPARC64%" "OS_TARGET=linux" "CPU_TARGET=sparc64"
+fptime make all OVERRIDEVERSIONCHECK=1 %* -j "OPT=-O3" "OPTNEW=%MYOPTSPARC64%" "OS_TARGET=linux" "CPU_TARGET=sparc64"
