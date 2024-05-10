dc0e826ec005c878299e7e42b964d7090717ce1e
florian <florian@freepascal.org>
2024-05-10 22:54:29 +0200

    * updates

--- makem68k.bat
+++ makem68k.bat
@@ -1,1 +1,1 @@
-make PP=c:\fpc\%MYFPC%compiler\m68k\pp OPT=@c:\fpc\bin\fpc.cfg %1 %2 %3 %4 %5 %6 %7 %8 %9
+make PP=c:\fpc\%MYFPC%compiler\m68k\pp OPT=@c:\fpc\bin\fpc.cfg %*
--- makepp11.bat
+++ makepp11.bat
@@ -1,1 +1,1 @@
-make FPC=c:\fpc\%MYFPC%compiler\i386\pp PP=c:\fpc\compiler\i386\pp "OPT=@c:\fpc\bin\fpc.cfg" %1 %2 %3 %4 %5 %6 %7 %8 %9
+make FPC=c:\fpc\%MYFPC%compiler\i386\pp PP=c:\fpc\compiler\i386\pp "OPT=@c:\fpc\bin\fpc.cfg" %*
