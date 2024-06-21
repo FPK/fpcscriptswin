daf76db62c55732d819cc8fd6c69ef2df2ffb7e1
florian <florian@freepascal.org>
2024-06-21 22:38:00 +0200

    * updates

--- makeppx64.bat
+++ makeppx64.bat
@@ -1,1 +1,1 @@
-make FPC=c:\fpc\%MYFPC%compiler\x86_64\pp "OPT=@c:\fpc\bin\fpc.cfg" %1 %2 %3 %4 %5 %6 %7 %8 %9
+make FPC=c:\fpc\%MYFPC%compiler\x86_64\pp "OPT=@c:\fpc\bin\fpc.cfg" %*
--- makesparc.bat
+++ makesparc.bat
@@ -1,1 +1,1 @@
-make PP=c:\fpc\%MYFPC%compiler\sparc\pp %1 %2 %3 %4 %5 %6 %7 %8 %9
+make PP=c:\fpc\%MYFPC%compiler\sparc\pp %*
