7b0a8decfdb4c2a9febf9e514d5f0deddd7af89a
florian <florian@freepascal.org>
2024-06-25 22:56:44 +0200

    * updates

--- pp11rv64.bat
+++ pp11rv64.bat
@@ -1,1 +1,1 @@
-c:\fpc\%MYFPC%compiler\riscv64\pp @c:\fpc\bin\fpc.cfg -XP$FPCTARGET- %1 %2 %3 %4 %5 %6 %7 %8 %9
+c:\fpc\%MYFPC%compiler\riscv64\pp @c:\fpc\bin\fpc.cfg -XP$FPCTARGET- %*
--- pp11sparc64.bat
+++ pp11sparc64.bat
@@ -1,1 +1,1 @@
-c:\fpc\%MYFPC%compiler\sparc64\pp @c:\fpc\bin\fpc.cfg %1 %2 %3 %4 %5 %6 %7 %8 %9
+c:\fpc\%MYFPC%compiler\sparc64\pp @c:\fpc\bin\fpc.cfg %*
