7b750007ed3d57c41d90e0763baaf3707c729965
florian <florian@freepascal.org>
2024-06-26 22:50:55 +0200

    * updates

--- tavr.bat
+++ tavr.bat
@@ -1,6 +1,6 @@
 set start=%time%
-make distclean TEST_DELTEMP=1 FPC=%CD%\..\compiler\ppc386 TEST_FPC=%CD%\..\compiler\ppcrossavr V=1 "TEST_SUBARCH=avr5" "TEST_OPT=%MYTEST_OPTAVR%" %1 %2 %3 %4 %5 %6 %7 %8 %9 "TEST_VERBOSE=1"
-make full TEST_DELTEMP=1 FPCFPMAKE=%CD%\..\compiler\ppc386 TEST_FPC=%CD%\..\compiler\ppcrossavr V=1 "TEST_SUBARCH=avr5" "TEST_OPT=%MYTEST_OPTAVR% -XPavr-embedded- -Wpavrsim" EMULATOR=c:\fpc\avrsim\avrsim.exe %1 %2 %3 %4 %5 %6 -j
+make distclean TEST_DELTEMP=1 FPC=%CD%\..\compiler\ppc386 TEST_FPC=%CD%\..\compiler\ppcrossavr V=1 "TEST_SUBARCH=avr5" "TEST_OPT=%MYTEST_OPTAVR%" %* "TEST_VERBOSE=1"
+make full TEST_DELTEMP=1 FPCFPMAKE=%CD%\..\compiler\ppc386 TEST_FPC=%CD%\..\compiler\ppcrossavr V=1 "TEST_SUBARCH=avr5" "TEST_OPT=%MYTEST_OPTAVR% -XPavr-embedded- -Wpavrsim" EMULATOR=c:\fpc\avrsim\avrsim.exe %* -j
 set end=%time%
 echo %start%
 echo %end%
