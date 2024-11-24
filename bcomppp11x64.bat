d714b619006ea38456a7ccced3b80858038105eb
florian <florian@freepascal.org>
2024-11-24 19:24:46 +0100

    * updates

--- bcomppp11x64.bat
+++ bcomppp11x64.bat
@@ -3,7 +3,7 @@ echo Missing suffix
 ) else (
 rem call brtlpp11.bat
 cd c:\fpc\%MYFPC%compiler
-make distclean OS_TARGET=win64 FPC=c:\fpc\%MYFPC%compiler\x86_64\pp PP=c:\fpc\compiler\x86_64\pp "OPT=@c:\fpc\bin\fpc.cfg -al %MYOPTX64%"
+make distclean OS_TARGET=win64 FPC=c:\fpc\%MYFPC%compiler\x86_64\pp PP=c:\fpc\compiler\x86_64\pp "OPT=@c:\fpc\bin\fpc.cfg -al %MYOPTX64%" || exit /b %ERRORLEVEL%
 make all OS_TARGET=win64 FPC=c:\fpc\%MYFPC%compiler\x86_64\pp PP=c:\fpc\%MYFPC%compiler\x86_64\pp "OPT=@c:\fpc\bin\fpc.cfg -al %MYOPTX64%" -j
 cd ..
 rm -rf units.%1
--- bs64.bat
+++ bs64.bat
@@ -5,10 +5,8 @@ echo Missing suffix
 ) else (
 cd %MYFPCDIR%\%MYFPC%
 mkdir output.%1
-make distclean FPC=c:\pp\bin\i386-win32\ppcrossx64 OVERRIDEVERSIONCHECK=1 CPU_TARGET=x86_64 OS_TARGET=win64 CPU_SOURCE=x86_64 OS_SOURCE=win64 "CROSSOPT=%MYCROSSOPTX64% -al" "OPTNEW=%MYOPTX64% -al" -j
-if %ERRORLEVEL% neq 0 goto end
+make distclean FPC=c:\pp\bin\i386-win32\ppcrossx64 OVERRIDEVERSIONCHECK=1 CPU_TARGET=x86_64 OS_TARGET=win64 CPU_SOURCE=x86_64 OS_SOURCE=win64 "CROSSOPT=%MYCROSSOPTX64% -al" "OPTNEW=%MYOPTX64% -al" -j || exit /b %ERRORLEVEL%
 make all FPC=c:\pp\bin\i386-win32\ppcrossx64 OVERRIDEVERSIONCHECK=1 CPU_TARGET=x86_64 OS_TARGET=win64 CPU_SOURCE=x86_64 OS_SOURCE=win64 "CROSSOPT=%MYCROSSOPTX64% -al -XPx86_64-win64-" "OPTNEW=%MYOPTX64% -al -XPx86_64-win64-" -j
-rem if %ERRORLEVEL% neq 0 goto end
 for /r "rtl" %%P in ("*.s") do (
   cp %%P output.%1
 )
@@ -19,5 +17,3 @@ for /r "packages" %%P in ("*.s") do (
   cp %%P output.%1
 )
 )
-
-:end
