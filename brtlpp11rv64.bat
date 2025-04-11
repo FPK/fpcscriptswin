7f5a02ded72c6b440cc12c3f2a7e74bef849245c
florian <florian@freepascal.org>
2025-04-11 22:41:43 +0200

    * updates

--- brtlpp11rv64.bat
+++ brtlpp11rv64.bat
@@ -2,7 +2,7 @@ if "%~1"== "" (
 echo Missing suffix
 ) else (
 cd c:\fpc\%MYFPC%rtl\linux
-make distclean FPC=c:\fpc\%MYFPC%compiler\riscv64\pp PP=c:\fpc\compiler\riscv64\pp "OPT=@c:\fpc\bin\fpc.cfg -al %MYOPTRV64%"
+make distclean FPC=c:\fpc\%MYFPC%compiler\riscv64\pp PP=c:\fpc\compiler\riscv64\pp "OPT=@c:\fpc\bin\fpc.cfg -al %MYOPTRV64%" -j
 make all FPC=c:\fpc\%MYFPC%compiler\riscv64\pp PP=c:\fpc\compiler\riscv64\pp "OPT=@c:\fpc\bin\fpc.cfg -al %MYOPTRV64%" -j
 cd ..
 rm -rf units.%1
